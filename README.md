f933764d6d2686f8f498e80d874e80609a66dbd5
stringertheory <mike.stringer@datascopeanalytics.com>
2016-09-07 01:35:12 -0500

    updated with buttons

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # computer-art
+
+[Nees](https://en.wikipedia.org/wiki/Georg_Nees#Modern_generative_graphic_design),
+[Nake](https://en.wikipedia.org/wiki/Frieder_Nake),
+[Noll](https://en.wikipedia.org/wiki/A._Michael*Noll), 
+[Mohr](https://en.wikipedia.org/wiki/Manfred_Mohr), [Molnar](https://en.wikipedia.org/wiki/Vera*Moln%C3%A1r)
--- js/noll.js
+++ js/noll.js
@@ -1,42 +1,44 @@
-var SVG_ID = '#canvas';
-var N_X = 42;
-var N_Y = 42;
-var STROKE_WIDTH = 0.4;
-var P_VERTICAL = 0.3;
-var P_DRAW = 0.2;
-var GRID_JITTER = 0.5;
+function redraw_noll () {
+  var SVG_ID = '#noll-canvas';
+  var N_X = 42;
+  var N_Y = 42;
+  var STROKE_WIDTH = 0.4;
+  var P_VERTICAL = 0.3;
+  var P_DRAW = 0.2;
+  var GRID_JITTER = 0.5;
 
-var s = Snap(SVG_ID);
-s.attr({
-  viewBox: Snap.format('-1 -1 {max_x} {max_y}', {
-    max_x: N_X + 2,
-    max_y: N_Y + 2
-  })
-});
+  var s = Snap(SVG_ID);
+  s.clear();
+  s.attr({
+    viewBox: Snap.format('-1 -1 {max_x} {max_y}', {
+      max_x: N_X + 2,
+      max_y: N_Y + 2
+    })
+  });
 
-_.each(_.range(N_X), function (x) {
-  _.each(_.range(N_Y), function (y) {
-    var r_squared = Math.pow((N_X / 2 - x), 2) + Math.pow((N_Y / 2 - y), 2);
-    if (Math.sqrt(r_squared) <= N_X / 2 && Math.random() < P_DRAW) {
-      var l = s.line(x, y, x + 1 + 2 * Math.random(), y).attr({
-	stroke: 'black',
-	strokeWidth: STROKE_WIDTH,
-      });
-      l.transform(
-	Snap.format('t{x},{y}', {
-    	  x: GRID_JITTER * Math.random(),
-    	  y: GRID_JITTER * Math.random()
-	})
-      );
-      if (Math.random() < P_VERTICAL) {
+  _.each(_.range(N_X), function (x) {
+    _.each(_.range(N_Y), function (y) {
+      var r_squared = Math.pow((N_X / 2 - x), 2) + Math.pow((N_Y / 2 - y), 2);
+      if (Math.sqrt(r_squared) <= N_X / 2 && Math.random() < P_DRAW) {
+	var l = s.line(x, y, x + 1 + 2 * Math.random(), y).attr({
+	  stroke: 'black',
+	  strokeWidth: STROKE_WIDTH,
+	});
 	l.transform(
-	  Snap.format('r{angle},{x_center},{y_center}', {
+	  Snap.format('t{x},{y}', {
+    	    x: GRID_JITTER * Math.random(),
+    	    y: GRID_JITTER * Math.random()
+	  })
+	);
+	if (Math.random() < P_VERTICAL) {
+	  l.transform(Snap.format('r{angle},{x_center},{y_center}', {
     	    angle: 90,
     	    x_center: x,
     	    y_center: y
-	  })
-	);
+	  }));
+	}
       }
-    }
+    });
   });
-});
+}
+redraw_noll()
