41fdf357ac12c9e8cdb0d58a7a2422041c9b2aa5
stringertheory <mike.stringer@datascopeanalytics.com>
2016-12-28 23:59:10 -0600

    adding PNG button

--- js/utils.js
+++ js/utils.js
@@ -5,6 +5,7 @@ function makeSVG(id, n_x, n_y) {
   s.clear()
   var frame = (n_x + 1) / (FRAME_FACTOR + 1)
   s.attr({
+    width: 350,
     viewBox: Snap.format('{min_x} {min_y} {width} {height}', {
       min_x: 0 - frame,
       min_y: 0 - frame,
@@ -13,4 +14,20 @@ function makeSVG(id, n_x, n_y) {
     })
   })
   return s
+}
+
+function makePNG(svg_id) {
+  var svg = document.getElementById(svg_id);
+  svg.toDataURL("image/png", {
+    callback : function(data) {
+      var image = data.replace("image/png", "image/octet-stream");
+      var a = document.createElement('a');
+      var hash = md5(image).substring(0, 6)
+      a.download = svg_id.split('-')[0] + '-' + hash + '.png';
+      a.href = image
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+    }
+  });
 }
